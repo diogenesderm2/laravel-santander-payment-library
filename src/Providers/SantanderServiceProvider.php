09310bf80240aed590091f117d73205b4208eef1
Diogenes estevao <diogenestotal@gmail.com>
2025-09-04 21:26:13 -0300

    Remover config/santander.php
    Removendo config/santander.php

--- src/Providers/SantanderServiceProvider.php
+++ src/Providers/SantanderServiceProvider.php
@@ -7,24 +7,17 @@ class SantanderServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        // Registrar serviços se necessário
+        
     }
 
     public function boot()
     {
-        // Registrar o comando
         if ($this->app->runningInConsole()) {
             $this->commands([
                 SantanderInstallCommand::class,
             ]);
         }
 
-        // Publicar arquivos de configuração
-        $this->publishes([
-            __DIR__.'/../config/santander.php' => config_path('santander.php'),
-        ], 'santander-config');
-
-        // Publicar stubs de certificados se necessário
         $this->publishes([
             __DIR__.'/../stubs/certificates/' => base_path('storage/certificates/santander/'),
         ], 'santander-certificates');
